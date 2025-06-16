8455dc837e63407cfabee52f7e3eb483c113a52c
Nishant-66 <kumarnishant26346@gmail.com>
2025-06-16 22:22:26 +0530

    intital file setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
-const express=require('express');
-const app=express();
-
+const express = require('express');
+const app = express();
+const dotenv=require('dotenv');
+dotenv.config();
+const connectDB=require('./config/database.js');
 app.use((req,res)=>{
     res.send("Hello from server ");
 })
-
-app.listen(3000,()=>{
+connectDB().then(()=>{
+    console.log('Database connection established...')
+    app.listen(3000,()=>{
     console.log('Server is Successfully listening on Port 3000 ');
+})
+}).catch((err)=>{
+    console.error('Database cannot be connected !! ',err);
+
 })
