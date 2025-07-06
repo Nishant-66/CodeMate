33dd16a81f5ddc2bc5f45d7169c9c5c38a0d968c
Nishant-66 <kumarnishant26346@gmail.com>
2025-07-07 01:09:27 +0530

    Basic Routing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Home} from './Components/Home'
+import {Hero} from './Components/Hero'
+import {Login} from './Components/Login'
+import {Signup} from './Components/Signup'
+import {Feed} from './Components/Feed'
 function App() {
   return (
     <div>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home></Home>} >
+        <Route path="/" element={<Hero/>}/>
+        <Route path="/feed" element={<Feed/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>}/>
+        
+        </Route>
+       </Routes>
+      </BrowserRouter>
+      
+
+      
     </div>
   )
 }
