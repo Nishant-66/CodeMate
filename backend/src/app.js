a58e80667ec48ce4ca1a0525800fb456f850286a
Nishant-66 <kumarnishant26346@gmail.com>
2025-07-15 02:43:13 +0530

    cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,15 +4,18 @@ const dotenv=require('dotenv');
 dotenv.config();
 const connectDB=require('./config/database.js');
 const cookieParser=require('cookie-parser');
-
+const cors=require('cors')
 app.use(cookieParser());
 app.use(express.json());
 
 const port=process.env.PORT||3000;
 const authRouter = require("./routes/auth");
 const requestRouter = require("./routes/request");
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 app.use("/", authRouter);
-
 app.use("/", requestRouter);
 
 connectDB().then(()=>{
